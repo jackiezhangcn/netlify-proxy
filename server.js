01308c4dbca9c73dd3e46df59dc75e4752238c01
jackiezhangcn <fslzyj@hotmail.com>
2024-06-17 12:39:29 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  const url = "https://www.google.com" + req.url;
+  req.pipe(request(url)).pipe(res);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
